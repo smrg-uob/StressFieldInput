fdf627cab9876b9f3b9c2a0c3d46f0c11eb9fcc5
Xavier van Heule <59011468+xvheule@users.noreply.github.com>
2022-11-15 10:50:05 +0000

    Version bump

--- StressFieldInput/StressFieldInput_Plugin.py
+++ StressFieldInput/StressFieldInput_Plugin.py
@@ -104,7 +104,7 @@ toolset.registerGuiMenuButton(
     icon=None,
     kernelInitString='import StressFieldInput_Kernel',
     applicableModules=abaqusConstants.ALL,
-    version='1.1',
+    version='2.0',
     author='Xavier van Heule',
     description='A plugin to easily input arbitrary stress fields into Abaqus',
     helpUrl='https://github.com/smrg-uob/StressFieldInput/blob/master/README.md'
