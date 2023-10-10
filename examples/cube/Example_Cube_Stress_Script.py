9162162f4c997c28ecf0b35dc689c3bfe66b02bb
Xavier van Heule <59011468+xvheule@users.noreply.github.com>
2023-10-10 23:05:36 +0100

    Change example to sinusoidal stresses

--- examples/cube/Example_Cube_Stress_Script.py
+++ examples/cube/Example_Cube_Stress_Script.py
@@ -2,8 +2,9 @@
 import numpy as npy
 
 
-# In this example, a linearly varying stress state with Z is applied
-# The slopes of the linear variation are read from a file
+# In this example, a stress state, varying sinusoidally in, Z is applied
+# The amplitudes are read from a file
+
 # Since the ABAQUS plugin will run this script in its entirety once, all code outside of the "calculate_stress" method
 # will be ran, this, for instance can be used to read values from a file
 
@@ -15,10 +16,10 @@ def readValuesFromFile(file_name):
     fid.close()
     # remove all spaces
     txt = txt.replace(" ", "")
-    # extract x slope
-    val_x = extractValue(txt, "slope_x")
-    # extract y slope
-    val_y = extractValue(txt, "slope_y")
+    # extract x amplitude
+    val_x = extractValue(txt, "amp_x")
+    # extract y amplitude
+    val_y = extractValue(txt, "amp_y")
     # return the values
     return val_x, val_y
 
@@ -32,17 +33,20 @@ def extractValue(txt, target):
     return float(txt[i1:i2])
 
 
+# The width of the domain
+w = 40
+
 # Here we initialize the values that will be used inside the "calculate_stress" method
 values = readValuesFromFile('Example_Cube_Value_File.txt')
-slope_x = 1E6*values[0]
-slope_y = 1E6*values[1]
+amp_x = 1E6*values[0]
+amp_y = 1E6*values[1]
 
 
 # The "calculate_stress" method, which is what the ABAQUS plugin will call to determine the desired stress in the model
 def calculate_stress(part, x, y, z, prev_stress):
     # First, calculate the desired stress components at the given coordinates
-    s_xx = slope_x*z
-    s_yy = slope_y*z
+    s_xx = amp_x*npy.sin(z*2*npy.pi/w)
+    s_yy = amp_y*npy.sin(z*2*npy.pi/w)
     # Then, return the stress tensor (a tuple of size 6 containing the stress components [S11, S22, S33, S12, S13, S23])
     # since equilibration in previous iterations might have resulted in non-zero stresses for the other components,
     # we return all previous stress components, overwriting the target stress components we do care about
